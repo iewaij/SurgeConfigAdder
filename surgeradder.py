2bbc60f6fba9f24abdefc3f0ddc62a8693fa5cfa
Jiawei Li <li.jw@me.com>
2017-08-31 01:54:58 +0800

    Add defualt value proxyName

--- surgeradder.py
+++ surgeradder.py
@@ -5,9 +5,7 @@ import sys
 # 请声明 Surge 配置文件路径
 path = '/Users/USERNAME/Library/Mobile Documents/iCloud~run~surge/Documents/NAMEOFCOFIGFILE.conf' # Use this path if your config file in iCloud. 该路径为 iCloud Surge 文件夹路径。
 
-# Specify your FINAL rule, 'FINAL,DIRECT' as default
-# 请声明 Surge 文件中的 FINAL 语句, 默认为 'FINAL,DIRECT'
-def surgeAdder(path, finalRule = 'FINAL,DIRECT', inputDomain = sys.argv[1]):
+def surgeAdder(path, proxyName = 'Proxy', finalRule = 'FINAL,DIRECT', inputDomain = sys.argv[1]):
     with open(path, 'r+') as f:
         content = f.read()
         # Look for where the finalRule starts
@@ -17,7 +15,7 @@ def surgeAdder(path, finalRule = 'FINAL,DIRECT', inputDomain = sys.argv[1]):
         f.seek(ruleLoc,0)
 
         # Writing rule from input
-        f.write('DOMAIN-SUFFIX,%s,Proxy\n%s\n' %(inputDomain, finalRule))
+        f.write('DOMAIN-SUFFIX,%s,%s\n%s\n' %(inputDomain, proxyName, finalRule))
     print('Add %s to config file successfully!' %inputDomain)
 
 surgeAdder(path)
